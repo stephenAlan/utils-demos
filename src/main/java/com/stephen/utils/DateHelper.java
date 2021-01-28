06bf4b8982e63f7558ede2d8b2b4e9f4bee031d3
shouchao.shi <stephenAlan@163.com>
2021-01-28 15:13:51 +0800

    日期相关工具类

---
+++ src/main/java/com/stephen/utils/DateHelper.java
@@ -0,0 +1,116 @@
+package com.stephen.utils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by ssc on 2021-01-28 14:51 .
+ * Description: 日期相关工具类
+ */
+public class DateHelper {
+
+    private static final ThreadLocal<DateFormat> dateFormat = new ThreadLocal<DateFormat>() {
+        @Override
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd");
+        }
+    };
+
+    private static final ThreadLocal<DateFormat> dateTimeFormat = new ThreadLocal<DateFormat>() {
+        @Override
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+    };
+
+    private static final ThreadLocal<DateFormat> yearMonthFormat = new ThreadLocal<DateFormat>() {
+        @Override
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM");
+        }
+    };
+
+    public static String formatDate(Date date) {
+        return dateFormat.get().format(date);
+    }
+
+    public static String formatDateTime(Date date) {
+        return dateTimeFormat.get().format(date);
+    }
+
+    public static String formatYearMonth(Date date) {
+        return yearMonthFormat.get().format(date);
+    }
+
+    public static Date parseDate(String str) throws ParseException {
+        return dateFormat.get().parse(str);
+    }
+
+    public static Date parseDateTime(String str) throws ParseException {
+        return dateTimeFormat.get().parse(str);
+    }
+
+    public static Date parseYearMonth(String str) throws ParseException {
+        return yearMonthFormat.get().parse(str);
+    }
+
+    public static Date toDate(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+
+    public static Date dateAddDays(Date date, int days) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_MONTH, days);
+        return calendar.getTime();
+    }
+
+    public static Date dateAddMonths(Date date, int months) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, months);
+        return calendar.getTime();
+    }
+
+    /**
+     * 是否是同一天
+     */
+    public static boolean sameDay(Date date1, Date date2) {
+        return org.apache.commons.lang3.time.DateUtils.isSameDay(date1, date2);
+    }
+
+    /**
+     * 是否是同样的年月
+     */
+    public static boolean sameYearMonth(Date date1, Date date2) {
+        if (null == date1 || null == date2) {
+            return false;
+        }
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) && cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH);
+    }
+
+    /**
+     * 获取上个月的第一天
+     */
+    public static Date lastMonthFirstDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.MONTH,-1);
+        calendar.set(Calendar.DAY_OF_MONTH,1);
+        calendar.set(Calendar.HOUR_OF_DAY,0);
+        calendar.set(Calendar.MINUTE,0);
+        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.MILLISECOND,0);
+        return calendar.getTime();
+    }
+
+}
