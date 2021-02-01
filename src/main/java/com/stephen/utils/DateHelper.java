68bb1e871cb20a04bad55245ce45bab6c1e5d83f
shouchao.shi <stephenAlan@163.com>
2021-02-01 11:03:59 +0800

    获取指定日期下个月的最后一天

--- src/main/java/com/stephen/utils/DateHelper.java
+++ src/main/java/com/stephen/utils/DateHelper.java
@@ -113,4 +113,19 @@ public class DateHelper {
         return calendar.getTime();
     }
 
+    /**
+     * 获取指定日期下个月的最后一天
+     * @return
+     */
+    public static Date nextMonthLastDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH,1);
+        calendar.set(Calendar.DAY_OF_MONTH,calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        calendar.set(Calendar.HOUR_OF_DAY,23);
+        calendar.set(Calendar.MINUTE,59);
+        calendar.set(Calendar.SECOND,59);
+        return calendar.getTime();
+    }
+
 }
